42297ecbf1a54298da7a0279694c67fd14d6b3da
1112487607 <1112487607@SC309C06.academico.icesi.edu.co>
2016-04-07 10:13:41 -0500

    hbhh

--- Filtros_S11_2/src/Ejecutable.java
+++ Filtros_S11_2/src/Ejecutable.java
@@ -3,14 +3,26 @@ import processing.core.PApplet;
 public class Ejecutable extends PApplet {
 	
 	Logica log;
+	float posX=100;
+	float posY;
+	float grav;
+	float speed;
 	
 	public void setup(){
 		size(1000,700);
 		log = new Logica();
+		grav= (float) 0.2;
 	}
 	public void draw(){
 		background(255); //Aiuda
 		fill(255,8,100);
-		ellipse(100, 100, 100, 100);
+		ellipse(posX, posY, 100, 100);
+		
+		posY=posY+speed;
+		speed=speed+grav;
+		
+		if (posY>height) {
+			
+		}
 	}
 }
